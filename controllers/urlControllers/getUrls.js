79f015bce03e0ed8fb21bf7977d66e5a819e9ea8
PauloBazan <paulobazan155@gmail.com>
2023-03-17 23:43:29 +0100

    fixed urse acces

--- controllers/urlControllers/getUrls.js
+++ controllers/urlControllers/getUrls.js
@@ -3,7 +3,7 @@ const UrlsManager = require("../../models/urlsManager");
 const getAll = async (req, res) => {
   const { username } = req.user;
   try {
-    const urlsData = await UrlsManager.getAllUrls(username);
+    const urlsData = await UrlsManager.getAllUrls(username.toLowerCase());
     res.status(200).json({ urlsData });
   } catch (error) {
     res.status(404).json({ error: error });
